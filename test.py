6872f406f205c639d559fb4e382f358b8ceea8b5
Juan Ignacio Fernandez <jruloignacio@hotmail.com>
2021-04-16 13:44:46 -0300

    Se corrigio test.py

--- test.py
+++ test.py
@@ -1,9 +1,30 @@
 
 import fileinput
 import re
+import io
+buffer = ''
 
-def verifyInvariants(file ):
+for i in open("log.txt","r"):
+   
+   algo = i.find("ice el disparo ")
+  
+   if(algo>0):
+     aux = i[algo+15:]
+     buffer = buffer+aux.rstrip('\n')
+    #  buffer = buffer+aux
+
+print(buffer)
+
+# fic = open("log2.txt", "w")
+# fic.write(buffer)
+# fic.close()
+
+# print(buffer);
+
+
+def verifyInvariants(buffer ):
 	i=0
+	file = io.StringIO(buffer)
 	line = [file.readline() , 0]
 	while(True):
 		
@@ -23,10 +44,38 @@ def verifyInvariants(file ):
 		print(line[0])
 	
 	#print(i)
-
-
-file = open("prueba.txt" , "r")
-
 print("Test de invariantes de transicion:")
-verifyInvariants(file)
+verifyInvariants(buffer)
 
+
+
+
+
+
+# def verifyInvariants(file ):
+# 	i=0
+# 	line = [file.readline() , 0]
+# 	while(True):
+		
+# 		line = re.subn('T0(.*?)((TA(.*?)((T6(.*?)T7(.*?)T2(.*?)T3(.*?)T4)|(T5(.*?)T2(.*?)T3)))|((T8(.*?)((T13(.*?)T14(.*?)T9(.*?)T10(.*?)T11)|(T12(.*?)T9(.*?)T10)))))',
+# 		'\g<1>\g<4>\g<7>\g<8>\g<9>\g<10>\g<12>\g<13>\g<16>\g<19>\g<20>\g<21>\g<22>\g<24>\g<25>', line[0].rstrip())
+# 		#print(line[0])
+        
+# 		i = i+1
+# 		if(line[1] == 0):
+# 			break
+		
+# 	line = re.subn('-' , '' , line[0].rstrip())
+# 	if(line[0] == ""):
+# 		print("El test ha finalizado exitosamente. Enhorabuena!")
+# 	else:
+# 		print("Error de T-Invariantes")
+# 		print(line[0])
+	
+# 	#print(i)
+
+
+# file = open("log2.txt" , "r")
+
+# print("Test de invariantes de transicion:")
+# verifyInvariants(file)
